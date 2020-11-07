9bce0c75f4752da8d8ae0c64b493b7d52e443fc7
SergiiSemeniuk <69sem@i.ua>
2020-11-07 17:10:54 +0200

    first commit

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTanjJDRT1AD4UW24cmSKa00VD-2zkpBDRH1g&usqp=CAU'></img>
+  </header>
+}
+
+export default Header;
